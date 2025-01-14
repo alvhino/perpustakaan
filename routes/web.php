5ef57529df6f75e47a4e82a5ea39d1ab0fc9c55e
niko685 <nikowinong2@gmail.com>
2025-01-14 21:32:34 +0700

    pembuatan route dan controller serta migrasi

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BukuController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('template.index');
 });
@@ -10,3 +12,21 @@ Route::get('/dashboard', function () {
     return view('dashboard.index');
 });
 
+
+Route::get('/login', [UserController::class, 'login']);
+
+
+Route::get('/buku', [BukuController::class, 'index']);
+Route::get('/buku/tambah', [BukuController::class, 'create']);
+Route::post('/buku/tambah', [BukuController::class, 'store']);
+Route::get('/buku/edit/{id}', [BukuController::class, 'edit']);
+Route::post('/buku/update/{id}', [BukuController::class, 'update']);
+Route::get('/buku/hapus/{id}', [BukuController::class, 'destroy']);
+
+
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/kategori/tambah', [KategoriController::class, 'create']);
+Route::post('/kategori/tambah', [KategoriController::class, 'store']);
+Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit']);
+Route::post('/kategori/update/{id}', [KategoriController::class, 'update']);
+Route::get('/kategori/hapus/{id}', [KategoriController::class, 'destroy']);
